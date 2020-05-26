13ca1013c0fbff22ff1d334d56204737ca7a9433
Robert Shearme <robert.shearme@bigpayme.com>
2020-05-26 18:25:41 +0100

    Changed the colour, added in saving but now using a big omega(n**2) algorithm

--- run.py
+++ run.py
@@ -1,72 +1,65 @@
-import pathlib
-
+import numpy as np
 import PySimpleGUI as sg
+import pathlib
+import sys
 
-sg.theme('DarkAmber')
+sg.theme('LightGreen6')
+SAVE = "save"
 
-def input_comparator(a, b):
-    input_choice = ""
+choice1, choice2 = sg.Button("A", key="A"), sg.Button("B", key="B")
+sort_layout = [
+    [sg.Text("Which is better?")],
+    [choice1, choice2],
+    [sg.Button(SAVE)]
+]
+
+def take_input(a, b):
     choice1.update(text=str(a))
     choice2.update(text=str(b))
+    input_choice = ""
     while input_choice not in ["A", "B"]:
         event, values = window.read()
+        if event == SAVE:
+            np.save(input_file.absolute().parent.joinpath(f"saved_{input_file.name}"), sorting_matrix)
         input_choice = event
-    return 1 if input_choice == "A" else -1
-
-# unsorted_list = ["1", "4", "2", "8", "7"]
-
-def merge_sort(l):
-    if len(l) == 1:
-        return l
-    midpoint = len(l) // 2
-    new_l = []
-    l1 = merge_sort(l[:midpoint])
-    l2 = merge_sort(l[midpoint:])
-    while l1 and l2:
-        comp_result = input_comparator(l1[0], l2[0])
-        if comp_result < 1:
-            new_l.append(l1[0])
-            l1 = l1[1:]
-        else:
-            new_l.append(l2[0])
-            l2 = l2[1:]
-
-    new_l.extend(l1)
-    new_l.extend(l2)
-    return new_l
-
-def check_sorting(sorted_list):
-    for index, elem in enumerate(sorted_list[:-1]):
-        comp_result = input_comparator(elem, sorted_list[index+1])
-        if comp_result > 0:
-            return 1
-    return 0
-
+    return -1 if input_choice == "A" else 1
 
 load_file_layout = [
     [sg.Text('Input', size=(8, 1)), sg.Input(), sg.FileBrowse()],
     [sg.Button("Sort ascending"), sg.Button("Sort descending")]
 ]
-event, load_file_values = sg.Window("Forbes' Comparator", load_file_layout).read(close=True)
+event, load_file_values = sg.Window("Comparatron3000", load_file_layout).read(close=True)
+rev = True if event == "Sort ascending" else False
 input_file = pathlib.Path(load_file_values[0])
 
-with open(input_file) as input_fh:
-    unsorted_list = input_fh.read().strip().split("\n")
-
-choice1, choice2 = sg.Button("A", key="A"), sg.Button("B", key="B")
-sort_layout = [
-    [sg.Text("Which do you prefer?")],
-    [choice1, choice2]
-]
-window = sg.Window("Forbes' Comparator", sort_layout, finalize=True)
-
-sorted_list = merge_sort(unsorted_list)
-check_result = check_sorting(sorted_list)
-if check_result != 0:
-    sg.popup("Forbes' Comparator", "Could not determine a ranking as your rankings were not consistent")
+if input_file.suffix == ".save":
+    sorting_matrix = np.load(input_file)
+elif input_file.suffix == ".txt":
+    with open(input_file) as input_fh:
+        unsorted_list = input_fh.read().strip().split("\n")
+    unsorted_list_len = len(unsorted_list)
+    sorting_matrix = np.zeros((unsorted_list_len, unsorted_list_len))
 else:
-    if event == "Sort descending":
-        sorted_list = sorted_list[::-1]
+    sg.popup("Invalid file type. File must be a .txt or .save file")
+    sys.exit()
 
-    with open(input_file.absolute().parent.joinpath(f"sorted_{input_file.name}"), "w") as output_fh:
-        output_fh.write("\n".join(sorted_list))
+window = sg.Window("Comparatron3000", sort_layout, finalize=True)
+
+for row_index, row in enumerate(sorting_matrix):
+    for column_index, column in enumerate(sorting_matrix):
+        if column_index <= row_index or sorting_matrix[row_index][column_index] != 0:
+            continue
+        column_value = unsorted_list[column_index]
+        row_value = unsorted_list[row_index]
+        # comparison_value = input(f"Do you prefer {column_value} or {row_value}?")
+        comparison_value = take_input(column_value, row_value)
+        sorting_matrix[column_index][row_index] = comparison_value
+        sorting_matrix[row_index][column_index] = -1 * comparison_value
+
+sorting_order = sorting_matrix.sum(axis=0)
+orderable_unsorted_list = zip(unsorted_list, sorting_order)
+sorted_result = sorted(orderable_unsorted_list, key= lambda x: x[1])
+if rev:
+    sorted_result = sorted_result[::-1]
+with open(input_file.absolute().parent.joinpath(f"sorted_{input_file.name}"), "w") as fh:
+    fh.write("\n".join([x[0] for x in sorted_result]))
